1f80d0f9a2cc5e8cd1027b70ae6da3205bc55264
cip <cip>
2019-06-11 15:14:56 +0300

    d munca

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Edit</h1>    
     {{version}}
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 import DataB from '../firebase/init.js'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  },
+  // components: {
+  //   HelloWorld
+  // },
   data(){
     return {
       version:""
